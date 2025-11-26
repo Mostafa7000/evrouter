f439bbf9a54c0bc0d43d1955f9a89109557831cb
Mostafa <mostafatarek7000@gmail.com>
2025-11-26 19:09:22 +0200

    Initial commit!

---
+++ src/main/java/com/ev/evrouter/EvRouterApplication.java
@@ -0,0 +1,13 @@
+package com.ev.evrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvRouterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ev/evrouter/EvRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ev.evrouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvRouterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
